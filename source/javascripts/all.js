1c43a5446216281511b5fb1938fe1a49985dad34
mikeybkats <michaelbkats@gmail.com>
2017-02-16 22:45:07 -0800

    nav working

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,19 @@
 // This is where it all goes :)
 //= require jquery
+
+// hamburger script
+$(document).ready(function(){
+  $('.hamburger').on('click', function(){
+    if($('.hamburger').hasClass('active')){
+      $('.nav-inner').animate({
+        left: "-310px"
+      });
+      $('.hamburger').removeClass('active');
+    }else {
+      $('.nav-inner').animate({
+        left: "0px"
+      });
+      $('.hamburger').addClass('active');
+    }
+  });
+});
