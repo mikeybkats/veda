4a78ba359a633f4e46cd0de25f2429d69943d759
mikeybkats <michaelbkats@gmail.com>
2017-04-08 09:34:28 -0700

    continuing to fix bugs

--- source/javascripts/hamburger.js
+++ source/javascripts/hamburger.js
@@ -1,7 +1,14 @@
 // hamburger script
+var hadClass = false;
 $(document).ready(function(){
   $('.hamburger').on('click', function(){
-    $('.mobile-menu').toggleClass('active')
+    $('.mobile-menu').toggleClass('active');
+    if ( $('body').hasClass('menu-hover') ){
+      hadClass = true;
+    }
+    if (hadClass === true){
+      $('body').toggleClass('menu-hover');
+    }
   });
   $('.navigation-links ul li a').on('click', function(){
     $('.mobile-menu').removeClass('active');
